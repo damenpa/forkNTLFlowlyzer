9cc085a501a0de2709f0c22d85e292d134d8750f
JessicaServer <jessicalizethborja@gmail.com>
2025-08-03 16:30:21 -0700

    make dirs cvsv write

--- NTLFlowLyzer/writers/csv_writer.py
+++ NTLFlowLyzer/writers/csv_writer.py
@@ -2,12 +2,23 @@
 
 import csv
 from .strategy import Strategy
+import os
 
 
 class CSVWriter(Strategy):
-    def write(self, file_address: str, data: list, writing_mode: str = 'w',
-            only_headers: bool = False) -> None:
-        with open(file_address, writing_mode, newline='') as f:
+    def write(
+        self,
+        file_address: str,
+        data: list,
+        writing_mode: str = "w",
+        only_headers: bool = False,
+    ) -> None:
+
+        dir_path = os.path.dirname(file_address)
+        if dir_path and not os.path.exists(dir_path):
+            os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_address, writing_mode, newline="") as f:
             writer = csv.writer(f)
             if len(data) == 0:
                 return
